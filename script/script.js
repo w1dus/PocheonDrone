06bd71125c22848a47e3ffcc5414550a059c6ca3
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-08-22 16:30:44 +0900

    자주 묻는 질문

--- script/script.js
+++ script/script.js
@@ -4,7 +4,34 @@ document.addEventListener("DOMContentLoaded", function(e){
   slideMenuToggle();
   mainSlide();
   main_keyword_slide();
+  subHeaderColor();
+  qnaToggle();
 })
+
+const qnaToggle = () => {
+  $('.sub.question .qna_list .box.q_box').click(function(){
+    $(this).closest('.item').toggleClass('show');
+    $(this).siblings('.a_box').slideToggle();
+  })
+}
+
+const subHeaderColor = () => {
+  const pageStyles = [
+    { className: "question", color: "#EA5E75" },
+  ];
+
+  const mainEl = document.querySelector("main");
+  const headerEl = document.querySelector("header");
+
+  if (mainEl && headerEl) {
+    pageStyles.forEach((rule) => {
+      if (mainEl.classList.contains(rule.className)) {
+        headerEl.style.background = rule.color;
+        headerEl.style.boxShadow = "unset";
+      }
+    });
+  }
+}
 
 const main_keyword_slide = () => {
   const inverseEl = document.querySelector(".inverse");
