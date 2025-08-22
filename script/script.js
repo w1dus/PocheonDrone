5543f352d55627c83e401789cc648482fc584dba
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-08-22 15:18:21 +0900

    메인 이미지 이슈

--- script/script.js
+++ script/script.js
@@ -2,8 +2,45 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
   slideMenuToggle();
+  mainSlide();
+  main_keyword_slide();
 })
 
+const main_keyword_slide = () => {
+  const inverseEl = document.querySelector(".inverse");
+  const reverseEl = document.querySelector(".reverse");
+
+  if (inverseEl) {
+    gsap.to(inverseEl, {
+      xPercent: -50, // 왼쪽으로 이동
+      duration: 40,
+      ease: "none",
+      repeat: -1
+    });
+  }
+
+  if (reverseEl) {
+    gsap.to(reverseEl, {
+      xPercent: 50, // 왼쪽으로 이동
+      duration: 40,
+      ease: "none",
+      repeat: -1
+    });
+  }
+};
+
+
+
+
+const mainSlide = () => {
+  var swiper = new Swiper(".main .slide_section .swiper", {
+    loop : true, 
+    pagination: {
+      el: ".main .slide_section .swiper-pagination",
+      clickable: true,
+    },
+  });
+}
 
 const slideMenuToggle = () => {
   $('header .menu_btn').click(function(){
