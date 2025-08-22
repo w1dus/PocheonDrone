c0fa1ab4a4635537d494d28446a47be969d65606
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-08-22 13:50:23 +0900

    헤더, 푸터 퍼블리싱

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+  slideMenuToggle();
 })
 
 
+const slideMenuToggle = () => {
+  $('header .menu_btn').click(function(){
+    $('.slide_menu').addClass('show');
+    $('.menu_close_bg').addClass('show');
+  })
+
+  $('.menu_close_bg').click(function(){
+    $('.slide_menu').removeClass('show');
+    $('.menu_close_bg').removeClass('show');
+  })
+
+  $('.slide_menu .btn_wrap .close_btn').click(function(){
+    $('.slide_menu').removeClass('show');
+    $('.menu_close_bg').removeClass('show');
+  })
+}
